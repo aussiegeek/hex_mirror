1afb07aa105357207e3455bab0b86cfe118773b6
David Parry <david.parry@suranyami.com>
2016-08-24 11:08:06 +1000

    cleaned up formatting to be more like https://github.com/lpil/dogma

--- mix.exs
+++ mix.exs
@@ -2,24 +2,35 @@ defmodule HexMirror.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :hex_mirror,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix, :gettext] ++ Mix.compilers,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :hex_mirror,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      elixirc_paths: elixirc_paths(Mix.env),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers,
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application.
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {HexMirror, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :httpoison, :hex,
-                   ]]
+    [
+      mod: {HexMirror, []},
+      applications: [
+        :phoenix,
+        :phoenix_pubsub,
+        :phoenix_html,
+        :cowboy,
+        :logger,
+        :gettext,
+        :httpoison,
+        :hex,
+      ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,14 +41,15 @@ defmodule HexMirror.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:httpoison, "~> 0.9.0"},
-     {:credo, "~> 0.4.5"},
+    [
+      {:phoenix, "~> 1.2.0"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:httpoison, "~> 0.9.0"},
+      {:credo, "~> 0.4.5"},
     ]
   end
 end
